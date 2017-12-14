398f46a76d889f8f9815a672b9919c24a2d49e97
Joel McLean <joel.mclean@micron21.com>
2017-12-14 18:38:45 +1100

    added some core files but I need to plan this in the README

---
+++ js/search-addon.js
@@ -0,0 +1,21 @@
+
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+	/* Stuff here */
+
+	    });
+}
+
+function reportExecuteScriptError(error) {
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute beastify content script: ${error.message}`);
+}
+
+browser.tabs.executeScript({file: "/content_scripts/search-addon.js"})
+.then(listenForClicks)
+.catch(reportExecuteScriptError);
